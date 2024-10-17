94ce4dd0e501c0be9d8ccfc08f86e236ba315968
Kokko00 <163394398+Kokko00@users.noreply.github.com>
2024-10-17 16:36:15 -0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_digiOTA user_project (
+  tt_um_anOTA user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
